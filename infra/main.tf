ef0652ea6b95173b6f3714c59e1e689d2b6f75b2
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-12-30 16:13:19 +0800

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -191,7 +191,7 @@ resource "aws_spot_instance_request" "frontend" {
   vpc_security_group_ids = [aws_security_group.frontend_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.frontend_profile.name
   
-//  spot_price                      = "0.02"
+  spot_price                      = "0.02"
   wait_for_fulfillment           = true
   spot_type                      = "persistent"
   instance_interruption_behavior = "stop"
@@ -245,12 +245,12 @@ resource "aws_spot_instance_request" "frontend" {
 # Backend EC2 Instance
 resource "aws_spot_instance_request" "backend" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "m5.large"
+  instance_type          = "m5.xlarge"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.backend_profile.name
 
-//  spot_price                     = "0.1"
+  spot_price                     = "0.09"
   wait_for_fulfillment           = true
   spot_type                      = "persistent"
   instance_interruption_behavior = "stop"
