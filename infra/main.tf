e42efc4f8c18b734fbb142dc6ad7084a30dae4f5
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-12-30 10:04:49 +0800

    fix: Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -245,7 +245,7 @@ resource "aws_spot_instance_request" "frontend" {
 # Backend EC2 Instance
 resource "aws_spot_instance_request" "backend" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "m5.xlarge"
+  instance_type          = "m5.large"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.backend_profile.name
