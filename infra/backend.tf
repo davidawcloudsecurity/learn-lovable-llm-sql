1d508a08128deffd5d05de6b8f63e70d6ce840bf
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-02-07 12:19:42 +0800

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "learn-lovable-llm-sql-tfm-state"
+    bucket         = "learn-lovable-llm-sql-2338-tfm-state"
     key            = "project1/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
